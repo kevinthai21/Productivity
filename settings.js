ff22e5c03d202bec7fe0569ebd8fc4dd80000e93
Kevin <kevin.thai21@gmail.com>
2020-09-22 11:18:16 -0700

    Can add and delete links now.

--- settings.js
+++ settings.js
@@ -3,21 +3,54 @@ setDefault();
 
 var userInput = document.getElementById("inputLink");
 var userEnter = document.getElementById("inputEnter");
+var userInputDelete = document.getElementById("inputDeleteLink");
+var userEnterDelete = document.getElementById("inputDeleteEnter");
+userEnter.addEventListener("click", addLink);
+userEnterDelete.addEventListener("click", deleteLink);
 
-userEnter.addEventListener("click", addLink(userInput.innerText));
-
-function addLink(userLink)
+function addLink()
 {  
-    console.log("Added " + userLink);
+    var inputValue = userInput.value;
+    console.log("<Attempting to add '" + inputValue + "'>");
+
+    if(inputValue == 0) {
+        alert("This is empty!");
+        return;
+    }
     storage.get("links", function(x)
     {
+        for(i in x.links) {
+            if(x.links[i] == inputValue) {
+                alert("It is already in the list!");
+                return;
+            }
+        }
         newLinks = x.links;
-        newLinks.push(userLink);
+        newLinks.push(inputValue);
         storage.set({"links": newLinks});
         console.log(x.links);
+        console.log("<Successfully added '" + inputValue + "'>");
     });
 }
 
+
+function deleteLink()
+{
+    var deleteValue = userInputDelete.value;
+    console.log("<Attempting to delete '" + deleteValue + "'>");
+    storage.get("links", function(x)
+    {
+        for(i in x.links) {
+            if(x.links[i] == deleteValue) {
+                x.links.splice(i,1);
+                console.log("<Successfully deleted '" + deleteValue + "'>");
+                return;
+            }
+        }
+    }
+
+}
+
 function setDefault() {
-    storage.set({"links": ['facebook.com','youtube.com', 'twitter.com']});
+    storage.set({"links": ['facebook.com','youtube.com','twitter.com']});
 }

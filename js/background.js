39e10db42649f60dc86ecf9bd1ff33d066d311a9
kevin.thai21 <kevin.thai21@gmail.com>
2020-09-28 20:18:10 -0700

    Added comments, change file paths for variables.

--- js/background.js
+++ js/background.js
@@ -1,74 +1,100 @@
-var storage = chrome.storage.local;
-var state, links, time;
-var totalSeconds;
-var blockedSites = ["facebook.com", "youtube.com", "twitter.com", 
-                        "linkedin.com", "instagram.com"];
-var blockedState = [true, true, true, true, true];
+/*
+ * File: background.js
+ * Created by Kevin Thai
+ * Date: September 30, 2020
+ *
+ * This file will do all the background processes needed for the extension.
+ * It contains a listener to check the URL of the sites that the user is
+ * requesting to look into. If the URL includes a site that is supposed to be
+ * blocked, the file would set the tab to the HTML page that says that it is
+ * blocked. The user can go back to their initial page if the user went to the
+ * URL page.
+ */
 
-var numDefaultBlocked = 5;
+var storage = chrome.storage.local; // the storage where data is saved.
+var state, links, time, totalSeconds; // local vars that hold storage values
 
 setDefault();
 setInterval(setTime, 1000);
 
-
-// Works fine, atm
+/*
+ * Function: blockSite();
+ * Description: It will update the active tab with the HTML page showing that
+ * the page is blocked.
+ */
 function blockSite() 
 {
-    // console.log("Found blocked site.");
     chrome.tabs.update({url: "blockedSite.html"})
 }
 
-function addLink() {
-    console.log("Added things.");
-    blockedSites.push(inputSite.innerHTML);
-    blockedSites.push(true);
-
-    console.log("Array: " + blockedSites);
-}
+/*
+ * Function: setDefault();
+ * Description: It will set the array to block major social media websites and
+ * it will save it into the local storage.
+ */
 function setDefault() {
     console.log("Set the default");
-    storage.set({"links": ['facebook.com','youtube.com','twitter.com', 'linkedin.com', 'instagram.com']});
+    storage.set({"links": ['facebook.com','youtube.com','twitter.com', 
+        'linkedin.com', 'instagram.com']});
 }
 
+
+/*
+ * Function: setTime();
+ * Description: It will take the data from the local storage and increase
+ * the number of seconds by 1. And set the final number into the local
+ * storage. This will be called every time a second is passed.
+ */
 function setTime()
 {
-    totalSeconds++;
+
     chrome.storage.local.get(["state","time"], function(data)
     {
+        // Gets data from the local storage
         state = data.state;
+	    totalSeconds = data.time;
 
+        // if not active productive session, then reset the session.
         if(!state) 
         {
             totalSeconds = 0;
             return;
         }
+	    totalSeconds++;
 
         storage.set({"time" : totalSeconds});
 
     });
 }
 
-// This method works but has some bugs.
+/*
+ * This is the listener that checks if the URL is part of the blocked list.
+ * If so, then it will update the tab. If not, it will continue as usual.
+ */
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-        // console.log(details)
         chrome.storage.local.get(["state", "links"], function(data) {
+
+            // Gets the data from the local storage.
             state = data.state;
             links = data.links;
 
+            // If not active productive session, then continue as normal.
             if(!state) return;
 
-
+            // checks every entry for a blocked URL.
             for(index=0; index< links.length; index++) {
-                if (details.url && state && 
-                    details.url.includes(links[index])) {
+
+                // check if there is a URL and if it should be blocked
+                if (details.url && details.url.includes(links[index])) {
+
+                        // This link shows when wanting to add a link to the blocked list
                         if (details.url.includes("settings.html?add_link=" + links[index])) {
                             return;
                         }
-                    // if(blockedState[index] == true) {
-                        console.log("Found a site: " + details.url);
+
+                        // This will update the tab to not go to the blocked URL.
                         blockSite();
-                    // }
                     return;
                 }
             }
--- js/popup.js
+++ js/popup.js
@@ -1,33 +1,63 @@
+/*
+ * File: popup.js
+ * Created by Kevin Thai
+ * September 30, 2020
+ *
+ * This file will change the information that popup displays depending
+ * on what the user does. When the user 'starts' the productive state,
+ * it will save it to the local storage and start the timer. If the user
+ * pressed the 'settings' button, it would lead to an HTML page that will
+ * help customize the user experience. The timer to give the user more 
+ * incentive to continue being productive.
+ */
+
+// Strings for the "state change" button.
 var stringOff = "Start";
 var stringOn = "Stop";
 
+// Variables that hold the elements from popup.html.
 var buttonState = document.getElementById("changeState");
 var buttonSetting = document.getElementById("settingsButton");
 var labelHour = document.getElementById("hours");
 var labelMinute = document.getElementById("minutes");
 var labelSeconds = document.getElementById("seconds");
 
+// Event listeners.
 buttonState.addEventListener("click", clickState);
 buttonSetting.addEventListener("click", clickSetting);
 
-console.log(document.URL);
 var storage = chrome.storage.local;
+
+// When displaying the popup, the file has to get the state data
+// in order to keep consistency.
 storage.get("state", function(x) {
     if( x.state == true) 
     {
         buttonState.innerText = stringOn;
+	changeTimeLabels();
     }
     else buttonState.innerText = stringOff;
 });
+
+// Calling this function every second.
 setInterval(changeTimeLabels, 1000);
 
+
+/*
+ * Function: clickState();
+ * Description: When the user clicks on the button to change
+ * the state between productive and non-productive, it
+ * will set the state in local storage. It will also change
+ * the inner text of the button to indicate the change of
+ * states is successful.
+ */
 function clickState()
 {
     storage.get(["state","time"], function(x) 
     {
         var newState;
-        console.log("State before: " + x.state);
-
+	
+	// if the state is off, then turn it on (and vice versa).
         if(x.state == false || x.state == undefined) 
         {
             changeTimeLabels();
@@ -42,26 +72,41 @@ function clickState()
             resetTimeLabels();
         }
 
-        console.log("State afer: " + newState);
         storage.set({"state": newState});
     });
 
 }
 
+/*
+ * Function clickSetting();
+ * Description: It opens a new tab to go to the HTML that the user
+ * can change the settings with.
+ */
 function clickSetting()
 {
     chrome.tabs.create({url: "settings.html"});
 }
 
+
+/*
+ * Function: changeTimeLabels();
+ * Description: The function looks into the data to determine the
+ * number of seconds has passed since activating the productivity
+ * state. It will change the time labels in the popup.
+ */
 function changeTimeLabels()
 {
     storage.get(["time", "state"], function(data) 
     {
+	// if not productive state, then don't change anything
         if(!data.state) return;
 
-        var seconds = parseInt(data.time%60);
+        // distinguish between seconds, minutes, hours from number of seconds
+	var seconds = parseInt(data.time%60);
         var minutes = parseInt((data.time/60)%60);
         var hours = parseInt(data.time/1200);
+	
+	// write out the numbers onto the popup.
         if(seconds<10) 
         {
             labelSeconds.innerText = "0" + seconds;
@@ -79,6 +124,11 @@ function changeTimeLabels()
     });
 }
 
+/*
+ * Function: resetTimeLabels();
+ * Description: When the user decides to turn of the productivity state,
+ * then the time labels will be reset.
+ */
 function resetTimeLabels()
 {
     labelSeconds.innerText = "";
